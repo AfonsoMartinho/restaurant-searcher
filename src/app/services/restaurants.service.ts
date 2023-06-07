dfda6cf893c94fe1e39cedf122f9929492a169b7
AfonsoMartinho <am.devlops@gmail.com>
2023-06-07 20:34:43 +0100

    feat: restaurant services now parses data from api

--- src/app/services/restaurants.service.ts
+++ src/app/services/restaurants.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +9,25 @@ export class RestaurantsService {
 
   constructor(private http: HttpClient) { }
 
-  getRestaurantsData(postCode: string): Observable<any[]> {
-    return this.http.get<any[]>(`/api/restaurants/bypostcode/${postCode}`, {
+  getRestaurantsData(postCode: string, page: number, itemsPerPage = 18): Observable<any>{
+    return this.http.get<any>(`/api/restaurants/bypostcode/${postCode}`, {
       headers: new HttpHeaders()
         .set( 'Content-Type', 'application/json'),
-    });
+    })
+    .pipe(map(data => {
+      const openRestaurants = data.Restaurants.map((restaurant: any) => {
+        if (restaurant.IsOpenNow) {
+          return {
+            name: restaurant.Name,
+            rating : restaurant.RatingAverage,
+            imageUrl: restaurant.LogoUrl,
+            cuisineTypes: Object.values({...restaurant.CuisineTypes.map((type: any) => type.Name)}),
+          }
+        } return {}
+      });
+      // handling pagination
+      // TODO pass this logic to the component
+      return openRestaurants.splice((page*itemsPerPage) - itemsPerPage, itemsPerPage )
+    }))
   }
 }
