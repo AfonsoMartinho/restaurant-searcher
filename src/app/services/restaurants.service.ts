d37c42ac3a1bb8dfb968c9fa529422752f82edd5
AfonsoMartinho <am.devlops@gmail.com>
2023-06-08 01:50:25 +0100

    chore: removed pagination handler from service

--- src/app/services/restaurants.service.ts
+++ src/app/services/restaurants.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 
@@ -9,25 +9,22 @@ export class RestaurantsService {
 
   constructor(private http: HttpClient) { }
 
-  getRestaurantsData(postCode: string, page: number, itemsPerPage = 18): Observable<any>{
+  getRestaurantsData(postCode: string): Observable<any>{
     return this.http.get<any>(`/api/restaurants/bypostcode/${postCode}`, {
       headers: new HttpHeaders()
         .set( 'Content-Type', 'application/json'),
     })
     .pipe(map(data => {
-      const openRestaurants = data.Restaurants.map((restaurant: any) => {
-        if (restaurant.IsOpenNow) {
+      const openRestaurants = data.Restaurants.filter((restaurant:any) => restaurant.IsOpenNow)
+      console.log(openRestaurants);
+      return openRestaurants.map((restaurant: any) => {
           return {
             name: restaurant.Name,
             rating : restaurant.RatingAverage,
             imageUrl: restaurant.LogoUrl,
             cuisineTypes: Object.values({...restaurant.CuisineTypes.map((type: any) => type.Name)}),
           }
-        } return {}
       });
-      // handling pagination
-      // TODO pass this logic to the component
-      return openRestaurants.splice((page*itemsPerPage) - itemsPerPage, itemsPerPage )
     }))
   }
 }
